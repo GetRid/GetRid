392d51feadc0f60ed9e106b39887b711ff0fb9f6
Craig Kuriger & Jonathan Bolo <pair+craigkuriger.jonathanbolo@devbootcamp.com>
2015-06-05 16:06:10 +1200

    knockout js in progress

--- GetRid/Frontend/js/dummydata.js
+++ GetRid/Frontend/js/dummydata.js
@@ -1,35 +1,34 @@
 $(document).ready(function() {
-
-testData = [
-  {
-    "name": "Fridge",
-    "description": "Random Words",
-    "location": "Te Aro",
-    "category": "Household Appliances",
-    "image": "https://www.topclasscarpentry.com/images/thumbnails/Fridges-Freezers/deluxe-frost-free-american-fridge-freezer-open_300.jpg"
-  },
-  {
-    "name": "Frezer",
-    "description": "More Random Words",
-    "location": "Te Aro",
-    "category": "Household Appliances",
-    "image": "http://placehold.it/300x100"
-  }
-]
-
-
+ko.applyBindings({
+  testData: [
+    {
+      "name": "Fridge",
+      "description": "Random Words",
+      "location": "Te Aro",
+      "category": "Household Appliances",
+      "image": "https://www.topclasscarpentry.com/images/thumbnails/Fridges-Freezers/deluxe-frost-free-american-fridge-freezer-open_300.jpg"
+    },
+    {
+      "name": "Frezer",
+      "description": "More Random Words",
+      "location": "Te Aro",
+      "category": "Household Appliances",
+      "image": "http://thefreezerguide.com/wp-content/uploads/2012/10/frigidaire-ffc0923dw-chest-freezer.jpg"
+    }
+  ]
+});
 
 $(document).on('click', '#tester', function(e){
   e.preventDefault();
   var $things = testData
+
+
   for (var i=0; i<testData.length; i+=1){
     function listView(){
       this.name = ko.observable(testData[i].name)
       this.image = ko.observable(testData[i].image)
-
-
-
     }
+
     ko.applyBindings(new listView());
 
 
@@ -37,6 +36,7 @@ $(document).on('click', '#tester', function(e){
     //   testData[i].name + '<br>'
       // )
   };
+
 
 });
 
