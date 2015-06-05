de724e55dc29baba07c07da41b1175386b69bd0c
Craig Kuriger & Jonathan Bolo <pair+craigkuriger.jonathanbolo@devbootcamp.com>
2015-06-05 18:14:55 +1200

    knockout click binding working

--- GetRid/Frontend/js/dummydata.js
+++ GetRid/Frontend/js/dummydata.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
 
-ko.applyBindings({
-  testData: [
+// ko.applyBinding({
+// Use testData: [ to apply ko binding]
+  testData = [
     {
       "name": "Fridge",
       "description": "Random Words",
@@ -25,29 +26,29 @@ ko.applyBindings({
       "image": "http://www.mustknowhow.com/wp-content/uploads/2010/05/fff37f7a-5e1d-49f7-b12f-7f2899effb0b_300.jpg"
     }
   ]
-});
+// });
 
 $(document).on('click', '#tester', function(e){
   e.preventDefault();
-  var $things = testData
-
-
-  for (var i=0; i<testData.length; i+=1){
-    function listView(){
-      this.name = ko.observable(testData[i].name)
-      this.image = ko.observable(testData[i].image)
-    }
-
-    ko.applyBindings(new listView());
-
-
-    // $('#appender').append(
-    //   testData[i].name + '<br>'
-      // )
-  };
-
-
+   function listView(){
+    testData
+   }
+  ko.applyBindings(new listView());
 });
+
+
+
+// $(document).on('click', '#tester', function(e){
+//   e.preventDefault();
+//   var $things = testData
+//   for (var i=0; i<testData.length; i+=1){
+//     function listView(){
+//       this.name = ko.observable(testData[i].name)
+//       this.image = ko.observable(testData[i].image)
+//     }
+//     ko.applyBindings(new listView());
+//   };
+// });
 
 
 
