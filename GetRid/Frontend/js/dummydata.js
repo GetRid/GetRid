0e04f33c6c3ad22e87693436c02379b4d5243f23
Craig Kuriger & Jonathan Bolo <pair+craigkuriger.jonathanbolo@devbootcamp.com>
2015-06-06 18:55:09 +1200

    cleanup UI

--- GetRid/Frontend/js/dummydata.js
+++ GetRid/Frontend/js/dummydata.js
@@ -25,116 +25,80 @@ $(document).ready(function() {
   ]
 
 
-function appViewModel(){
-  var self = this;
-  self.showSplashScreen = ko.observable(true);
-  //self.displays = ['All', 'Categories', 'Location', 'Get Rid'];
+  function appViewModel(){
+    var self = this;
+    self.showSplashScreen = ko.observable(true);
+    self.showGetRidForm = ko.observable(false);
+    //self.displays = ['All', 'Categories', 'Location', 'Get Rid'];
 
-  self.displays = [{label : "All",
-                    icon: "fa fa-globe"
-                   },
-                   {label : "Category",
-                    icon: "fa fa-folder-open-o"
-                   },
-                   {label : "Search Location",
-                    icon: "fa fa-location-arrow"
-                   },
-                   {label : "Get Rid",
-                    icon: "fa fa-trash-o"
-                   },
-                   {label : "jbolo",
-                    icon  : "fa fa-fw fa-user"}
-                  ];
-  self.chosenDisplayId = ko.observable();
-  self.chosenDisplayData = ko.observable();
-  self.chosenIndividualData = ko.observable();
-  self.chosenIndividualDetails = ko.observable();
-  self.makeContact = ko.observable();
+    self.displays = [{label : "All"},
+                     {label : "Category"},
+                     {label : "Search Location"},
+                     {label : "Get Rid"},
+                    ];
+    self.chosenDisplayId = ko.observable();
+    self.chosenDisplayData = ko.observable();
+    self.chosenIndividualData = ko.observable();
+    self.chosenIndividualDetails = ko.observable();
+    self.makeContact = ko.observable();
+    self.getRidData = ko.observable();
 
 
-  //Behaviours
-  self.browseNearYou = function(display) {
-    self.showSplashScreen(false);
-    self.goToDisplay(display);
-  }
+    //Behaviours
+    self.browseNearYou = function(display) {
+      self.showSplashScreen(false);
+      self.goToDisplay(display);
+    }
 
-  self.goToDisplay = function(display) {
-    self.chosenDisplayId(display)
-    self.chosenIndividualData(null);
-    self.chosenIndividualDetails(null);
-    //$.get('/items#####', {param: all}, self.chosenDisplayData)
-    self.chosenDisplayData(testData);
-    self.goToItem(testData[0]);
-  };
+    self.goToDisplay = function(display) {
+      self.chosenDisplayId(display)
+      self.chosenIndividualData(null);
+      self.chosenIndividualDetails(null);
+      //$.get('/items#####', {param: all}, self.chosenDisplayData)
+      self.chosenDisplayData(testData);
+      self.goToItem(testData[0]);
+    };
 
-  self.trash = function(){
-    testData.shift();
-     self.goToItem(testData[0]);
-  }
+    self.trash = function(){
+      testData.shift();
+       self.goToItem(testData[0]);
+    }
 
-  self.treasure = function(){
-    self.goToDetails(testData[0]);
-  }
+    self.treasure = function(){
+      self.goToDetails(testData[0]);
+    }
 
-  self.goToItem = function(item) {
-    self.chosenDisplayId(item.display);
-    self.chosenDisplayData(null);
-    //$.get('item', {itemId: testData.id}, self.chosenIndividualData)
-    self.chosenIndividualData(item);
-  }
+    self.goToItem = function(item) {
+      self.chosenDisplayId(item.display);
+      self.chosenDisplayData(null);
+      //$.get('item', {itemId: testData.id}, self.chosenIndividualData)
+      self.chosenIndividualData(item);
+    }
 
-  self.goToDetails = function(item){
-    self.chosenIndividualData(null);
-    self.chosenIndividualDetails(item)
-  }
+    self.goToDetails = function(item){
+      self.chosenIndividualData(null);
+      self.chosenIndividualDetails(item)
+    }
 
-  // self.makeContact = function(item){
-  //   self.chosenIndividualDetails(null)
-  //   self.makeContact(item)
-  // }
+    self.goToGetRid = function() {
+      self.chosenDisplayId(null)
+      self.chosenIndividualData(null);
+      self.chosenIndividualDetails(null);
+      self.chosenDisplayData(null);
+      self.getRidData();
+      self.showSplashScreen(false);
+      self.showGetRidForm(true);
+    }
 
-  //self.goToDisplay('All');
-  //self.goToItem(testData[0]);
+    // self.makeContact = function(item){
+    //   self.chosenIndividualDetails(null)
+    //   self.makeContact(item)
+    // }
 
-}; //End of appViewModel
-ko.applyBindings(new appViewModel());
+    //self.goToDisplay('All');
+    //self.goToItem(testData[0]);
 
-// $(document).on('click', '#list', function(e){
-//   e.preventDefault();
-//    function listView(){
-//     // if (testData.length > 0){
-//     //   testData: ko.observable(true)
-//     //     } else {
-//     //   testData: ko.observable(false)
-//     //     }
-//     testData
-//    }
-//   ko.applyBindings(new listView());
-// });
-
-// $(document).on('click', '#swipe', function(e){
-//   e.preventDefault();
-//     function swipeView(){
-//       swipeTestData
-//     }
-//   ko.applyBindings(new swipeView());
-// });
-
-
-
-// $(document).on('click', '#tester', function(e){
-//   e.preventDefault();
-//   var $things = testData
-//   for (var i=0; i<testData.length; i+=1){
-//     function listView(){
-//       this.name = ko.observable(testData[i].name)
-//       this.image = ko.observable(testData[i].image)
-//     }
-//     ko.applyBindings(new listView());
-//   };
-// });
-
-
-
+  }; //End of appViewModel
+  ko.applyBindings(new appViewModel());
 
 }); //End of doc ready
